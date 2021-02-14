9b582d556ec13f2cdef7ab98b868190f7ed2501c
MbhanuS <bhanusengar24@gmail.com>
2021-02-14 23:13:42 +0530

    added card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'anganamorey out now',
+      image: 'assets/images/sg1.jpg',
+      userName: 'shreyaghoshal',
+      content: `And its yours now!
+    #AnganaMorey - Out now
+    An electro pop bandish in collaboration with 
+    @soumghoshal
+     Hope you enjoy this audio visual treat!
+    
+    https://youtu.be/vKkzozVVuKY`,
+    },
+    {
+      title: 'instagram filter',
+      image: 'assets/images/sg2.jpg',
+      userName: 'shreyaghoshal',
+      content: `Yeah!! We came out with this special #AnganaMorey
+        Instagram filter!! Go check it out and make your IG reels/stories with it!Heart suitButterfly`,
+    },
+    {
+      title: 'shreya ghoshal dp',
+      image: 'assets/images/sg3.jpg',
+      userName: 'TheQuint',
+      content: `Catch musical sensation 
+      @shreyaghoshal
+       LIVE with 
+      @AbiraDhar
+       at 4 pm on 8 February, where she talks about her
+        journey and what the audience can look forward to!`,
+    },
+  ];
 }
